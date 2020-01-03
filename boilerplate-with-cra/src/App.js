47fed03f8cb75f12e85a1e5ce04fe5ac1d42ef76
daae <daae@daaeui-MacBookPro.local>
2020-01-03 18:10:33 +0900

    cra로 만들고 개발환경설정

---
+++ boilerplate-with-cra/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'antd';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">버튼</Button>
+    </div>
+  );
+}
+
+export default App;

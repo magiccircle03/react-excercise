d04f312caebad8d8344781466c4bca391d0bbb19
daae <daae@daaeui-MacBookPro.local>
2020-01-07 10:51:52 +0900

    update main.js

--- boilerplate/src/main.js
+++ boilerplate/src/main.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import AppLayout from './components/AppLayout';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+//ReactDOM.render(<App />, document.getElementById('app'));
 ReactDOM.render(<AppLayout />, document.getElementById('appLayout'));
